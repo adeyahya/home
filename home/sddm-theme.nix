47c4ff95da632cd9b72cc7e82b181e33e8eb79b0
adeyahya <adeyahyaprasetyo@gmail.com>
2023-10-08 06:27:01 +0700

    sddm theme

--- home/sddm-theme.nix
+++ home/sddm-theme.nix
@@ -2,15 +2,14 @@
 
 pkgs.stdenv.mkDerivation {
   name = "sddm-theme";
-  src = pkgs.fetchFromGithub {
+  src = pkgs.fetchFromGitHub {
     owner = "catppuccin";
     repo = "sddm";
     rev = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
-    sha256 = "7fc67d1027cdb7f4d833c5d23a8c34a0029b0661";
+    sha256 = "0spqrdppyzy2c31mv77gvcl52wwqk34y8ix5gahgdizg9g4k0dja";
   };
-
   installPhase = ''
     mkdir -p $out
-    cp -R ./src/* $out/
+    cp -R src/catppuccin-mocha/* $out/
   '';
 }
